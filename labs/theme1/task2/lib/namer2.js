34f6bbe948273fab3266d660b97bc6a3101da63c
Andrew <43204345+texasdog@users.noreply.github.com>
2020-07-02 22:30:31 +0300

    Update namer2.js

--- labs/theme1/task2/lib/namer2.js
+++ labs/theme1/task2/lib/namer2.js
@@ -6,20 +6,21 @@ Object.defineProperty(exports, "__esModule", {
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Can't call a class as a function"); } }
 
 var _class = function () {
     function _class() {
         _classCallCheck(this, _class);
 
-        this.first = 'Nikita';
-        this.last = 'Shimko';
+        this.last = 'Andrew';
+        this.middle = 'Sergeevich';
+        this.first = 'Anisimov';
     }
 
     _createClass(_class, [{
         key: 'getFullName',
         value: function getFullName() {
-            return this.first + ' ' + this.last;
+            return this.last + ' ' + this.middle + ' ' + this.first;
         }
     }]);
 
