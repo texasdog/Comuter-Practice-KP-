2b0e67e04b27f7efa16d666be9caeb84ee4abe55
Andrew <anisimov-andrew-1999@mail.ru>
2020-07-02 22:16:47 +0300

    Initial commit

---
+++ labs/theme1/task2/src/namer2.js
@@ -0,0 +1,8 @@
+export default class {
+    first = 'Nikita';
+    last = 'Shimko'
+
+    getFullName(){
+        return `${this.first} ${this.last}`;
+    };
+};
---
+++ theme2/task1/.eslintrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
+  };
