606e6864c0cd0d074ad70f6e02bce1daa2ea0a66
Andrew <43204345+texasdog@users.noreply.github.com>
2020-07-02 22:36:33 +0300

    Update main.js

--- labs/theme1/task1/src/main.js
+++ labs/theme1/task1/src/main.js
@@ -1,4 +1,4 @@
 import moment from 'moment';
 import name from './name';
 console.log(name);
-console.log(moment.unix('1500514362').format('DD.MM.YYYY HH:mm:ss'));
+console.log(moment.unix('1500514362').format('DD.MM.YYYYHH:mm:ss'));
